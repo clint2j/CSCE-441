cca62ea75d5adfadc7a892a6455422f890141986
Clint Hart <clinthart212@gmail.com>
2024-09-23 00:34:55 -0500

    Slight changes

--- PA2/src/main.cpp
+++ PA2/src/main.cpp
@@ -1,14 +1,14 @@
 #define GLEW_STATIC
+#include "MatrixStack.h"
+#include "Program.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <vector>
 #include <iostream>
-#include <utility>
 #include <optional>
-#include "MatrixStack.h"
-#include "Program.h"
+#include <utility>
+#include <vector>
 using std::pair;
 
 using mousePos = std::optional<glm::vec2>;
@@ -30,226 +30,194 @@ Program program;
 MatrixStack modelViewProjectionMatrix;
 
 // Draw cube on screen
-void DrawCube(glm::mat4& modelViewProjectionMatrix)
+void DrawCube(glm::mat4 &modelViewProjectionMatrix)
 {
-	program.SendUniformData(modelViewProjectionMatrix, "mvp");
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+    program.SendUniformData(modelViewProjectionMatrix, "mvp");
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
 class RobotElements
 {
-private:
-
-public:
-	RobotElements() {}
-	~RobotElements() {}
+  private:
+  public:
+    RobotElements() {}
+    ~RobotElements() {}
 };
 
-
-
-
 void Display()
-{	
-	program.Bind();
+{
+    program.Bind();
 
-	modelViewProjectionMatrix.loadIdentity();
-	modelViewProjectionMatrix.pushMatrix();
+    modelViewProjectionMatrix.loadIdentity();
+    modelViewProjectionMatrix.pushMatrix();
 
-	// Setting the view and Projection matrices
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	modelViewProjectionMatrix.Perspective(glm::radians(60.0f), float(width) / float(height), 0.1f, 100.0f);
-	modelViewProjectionMatrix.LookAt(eye, center, up);
-	
-	// Model transformation for Cube 1
-	modelViewProjectionMatrix.pushMatrix();
-	modelViewProjectionMatrix.translate(-2.0f, 2.0f, 0.0f);
-	modelViewProjectionMatrix.rotateY(glm::radians(45.0f));
-	modelViewProjectionMatrix.scale(0.8);
-	DrawCube(modelViewProjectionMatrix.topMatrix());
-	modelViewProjectionMatrix.popMatrix();
-	
-	// Model transformation for Cube 2
-	modelViewProjectionMatrix.pushMatrix();
-	modelViewProjectionMatrix.translate(-2.0f, -2.0f, 0.0f);
-	modelViewProjectionMatrix.rotateX(glm::radians(45.0f));
-	modelViewProjectionMatrix.scale(0.8);
-	DrawCube(modelViewProjectionMatrix.topMatrix());
-	modelViewProjectionMatrix.popMatrix();
-	
-	// Model transformation for Cube 3
-	modelViewProjectionMatrix.pushMatrix();
-	modelViewProjectionMatrix.translate(2.0f, 0.0f, 0.0f);
-	modelViewProjectionMatrix.rotateZ(glm::radians(45.0f));
-	modelViewProjectionMatrix.scale(0.8);
-	DrawCube(modelViewProjectionMatrix.topMatrix());
-	modelViewProjectionMatrix.popMatrix();
+    // Setting the view and Projection matrices
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    modelViewProjectionMatrix.Perspective(glm::radians(60.0f), float(width) / float(height), 0.1f,
+                                          100.0f);
+    modelViewProjectionMatrix.LookAt(eye, center, up);
 
-	modelViewProjectionMatrix.popMatrix();
+    // Model transformation for Cube 1
+    modelViewProjectionMatrix.pushMatrix();
+    modelViewProjectionMatrix.translate(-2.0f, 2.0f, 0.0f);
+    modelViewProjectionMatrix.rotateY(glm::radians(45.0f));
+    modelViewProjectionMatrix.scale(0.8);
+    DrawCube(modelViewProjectionMatrix.topMatrix());
+    modelViewProjectionMatrix.popMatrix();
 
-	program.Unbind();
-	
+    // Model transformation for Cube 2
+    modelViewProjectionMatrix.pushMatrix();
+    modelViewProjectionMatrix.translate(-2.0f, -2.0f, 0.0f);
+    modelViewProjectionMatrix.rotateX(glm::radians(45.0f));
+    modelViewProjectionMatrix.scale(0.8);
+    DrawCube(modelViewProjectionMatrix.topMatrix());
+    modelViewProjectionMatrix.popMatrix();
+
+    // Model transformation for Cube 3
+    modelViewProjectionMatrix.pushMatrix();
+    modelViewProjectionMatrix.translate(2.0f, 0.0f, 0.0f);
+    modelViewProjectionMatrix.rotateZ(glm::radians(45.0f));
+    modelViewProjectionMatrix.scale(0.8);
+    DrawCube(modelViewProjectionMatrix.topMatrix());
+    modelViewProjectionMatrix.popMatrix();
+
+    modelViewProjectionMatrix.popMatrix();
+
+    program.Unbind();
 }
 
 // Mouse callback function
-void MouseCallback(GLFWwindow* lWindow, int button, int action, int mods)
+void MouseCallback(GLFWwindow *lWindow, int button, int action, int mods)
 {
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
-		std::cout << "Mouse left button is pressed." << std::endl;
-	else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE){
-		std::cout << "Mouse Released!!!!\n";
-		lastMousePos = {};
-	}
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        std::cout << "Mouse left button is pressed." << std::endl;
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+        std::cout << "Mouse Released!!!!\n";
+        lastMousePos = {};
+    }
 }
 
 // Mouse position callback function
-void CursorPositionCallback(GLFWwindow* lWindow, double xpos, double ypos)
+void CursorPositionCallback(GLFWwindow *lWindow, double xpos, double ypos)
 {
-	int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
-	glm::vec4 viewDirection = glm::vec4(glm::normalize(center - eye), 0.0f);
-	glm::vec3 right = glm::cross(glm::vec3(viewDirection), glm::vec3(up));
+    int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+    int state2 = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
+    glm::vec4 viewDirection = glm::vec4(glm::normalize(center - eye), 0.0f);
+    glm::vec3 right = glm::cross(glm::vec3(viewDirection), glm::vec3(up));
     right = glm::normalize(right);
-	if (state == GLFW_PRESS){
-		glm::vec2 currMouse = {xpos,ypos};
-		if (!lastMousePos){
-			lastMousePos = currMouse;
-		}
-		else {
-			glm::vec2 diff = currMouse-lastMousePos.value();
-			if (diff.x != 0){
-				glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(diff.x), up);
-				eye = glm::vec3(rotationMatrix * glm::vec4(eye, 1.0f));
-			}
-			else if (diff.y != 0){
-				glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(diff.y), right);
-				eye = glm::vec3(rotationMatrix * glm::vec4(eye, 1.0f));
-			}
-			lastMousePos = currMouse;
-		}
-		// std::cout << "Mouse position is: x - " << xpos << ", y - " << ypos << std::endl;
-	}
-	state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
-	if (state == GLFW_PRESS){
-		
-	}
+    glm::vec2 currMouse = {xpos, ypos};
+    // handle scroll zoom here
+    if (!lastMousePos) {
+        lastMousePos = currMouse;
+        return;
+    }
+    glm::vec2 diff = currMouse - lastMousePos.value();
+    if (state == GLFW_PRESS) {
+        if (diff.x != 0) {
+            glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(diff.x), up);
+            eye = glm::vec3(rotationMatrix * glm::vec4(eye, 1.0f));
+        } else if (diff.y != 0) {
+            glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(diff.y), right);
+            eye = glm::vec3(rotationMatrix * glm::vec4(eye, 1.0f));
+        }
+    } else if (state2 == GLFW_PRESS) {
+    }
+    lastMousePos = currMouse;
 }
 
-
 // Keyboard character callback function
-void CharacterCallback(GLFWwindow* lWindow, unsigned int key)
+void CharacterCallback(GLFWwindow *lWindow, unsigned int key)
 {
-	std::cout << "Key " << (char)key << " is pressed." << std::endl;
+    std::cout << "Key " << (char)key << " is pressed." << std::endl;
 }
 
 void CreateCube()
 {
-	// x, y, z, r, g, b, ...
-	float cubeVerts[] = {
-		// Face x-
-		-1.0f,	+1.0f,	+1.0f,	0.8f,	0.2f,	0.2f,
-		-1.0f,	+1.0f,	-1.0f,	0.8f,	0.2f,	0.2f,
-		-1.0f,	-1.0f,	+1.0f,	0.8f,	0.2f,	0.2f,
-		-1.0f,	-1.0f,	+1.0f,	0.8f,	0.2f,	0.2f,
-		-1.0f,	+1.0f,	-1.0f,	0.8f,	0.2f,	0.2f,
-		-1.0f,	-1.0f,	-1.0f,	0.8f,	0.2f,	0.2f,
-		// Face x+
-		+1.0f,	+1.0f,	+1.0f,	0.8f,	0.2f,	0.2f,
-		+1.0f,	-1.0f,	+1.0f,	0.8f,	0.2f,	0.2f,
-		+1.0f,	+1.0f,	-1.0f,	0.8f,	0.2f,	0.2f,
-		+1.0f,	+1.0f,	-1.0f,	0.8f,	0.2f,	0.2f,
-		+1.0f,	-1.0f,	+1.0f,	0.8f,	0.2f,	0.2f,
-		+1.0f,	-1.0f,	-1.0f,	0.8f,	0.2f,	0.2f,
-		// Face y-
-		+1.0f,	-1.0f,	+1.0f,	0.2f,	0.8f,	0.2f,
-		-1.0f,	-1.0f,	+1.0f,	0.2f,	0.8f,	0.2f,
-		+1.0f,	-1.0f,	-1.0f,	0.2f,	0.8f,	0.2f,
-		+1.0f,	-1.0f,	-1.0f,	0.2f,	0.8f,	0.2f,
-		-1.0f,	-1.0f,	+1.0f,	0.2f,	0.8f,	0.2f,
-		-1.0f,	-1.0f,	-1.0f,	0.2f,	0.8f,	0.2f,
-		// Face y+
-		+1.0f,	+1.0f,	+1.0f,	0.2f,	0.8f,	0.2f,
-		+1.0f,	+1.0f,	-1.0f,	0.2f,	0.8f,	0.2f,
-		-1.0f,	+1.0f,	+1.0f,	0.2f,	0.8f,	0.2f,
-		-1.0f,	+1.0f,	+1.0f,	0.2f,	0.8f,	0.2f,
-		+1.0f,	+1.0f,	-1.0f,	0.2f,	0.8f,	0.2f,
-		-1.0f,	+1.0f,	-1.0f,	0.2f,	0.8f,	0.2f,
-		// Face z-
-		+1.0f,	+1.0f,	-1.0f,	0.2f,	0.2f,	0.8f,
-		+1.0f,	-1.0f,	-1.0f,	0.2f,	0.2f,	0.8f,
-		-1.0f,	+1.0f,	-1.0f,	0.2f,	0.2f,	0.8f,
-		-1.0f,	+1.0f,	-1.0f,	0.2f,	0.2f,	0.8f,
-		+1.0f,	-1.0f,	-1.0f,	0.2f,	0.2f,	0.8f,
-		-1.0f,	-1.0f,	-1.0f,	0.2f,	0.2f,	0.8f,
-		// Face z+
-		+1.0f,	+1.0f,	+1.0f,	0.2f,	0.2f,	0.8f,
-		-1.0f,	+1.0f,	+1.0f,	0.2f,	0.2f,	0.8f,
-		+1.0f,	-1.0f,	+1.0f,	0.2f,	0.2f,	0.8f,
-		+1.0f,	-1.0f,	+1.0f,	0.2f,	0.2f,	0.8f,
-		-1.0f,	+1.0f,	+1.0f,	0.2f,	0.2f,	0.8f,
-		-1.0f,	-1.0f,	+1.0f,	0.2f,	0.2f,	0.8f
-	};
+    // x, y, z, r, g, b, ...
+    float cubeVerts[]
+        = {// Face x-
+           -1.0f, +1.0f, +1.0f, 0.8f, 0.2f, 0.2f, -1.0f, +1.0f, -1.0f, 0.8f, 0.2f, 0.2f, -1.0f,
+           -1.0f, +1.0f, 0.8f, 0.2f, 0.2f, -1.0f, -1.0f, +1.0f, 0.8f, 0.2f, 0.2f, -1.0f, +1.0f,
+           -1.0f, 0.8f, 0.2f, 0.2f, -1.0f, -1.0f, -1.0f, 0.8f, 0.2f, 0.2f,
+           // Face x+
+           +1.0f, +1.0f, +1.0f, 0.8f, 0.2f, 0.2f, +1.0f, -1.0f, +1.0f, 0.8f, 0.2f, 0.2f, +1.0f,
+           +1.0f, -1.0f, 0.8f, 0.2f, 0.2f, +1.0f, +1.0f, -1.0f, 0.8f, 0.2f, 0.2f, +1.0f, -1.0f,
+           +1.0f, 0.8f, 0.2f, 0.2f, +1.0f, -1.0f, -1.0f, 0.8f, 0.2f, 0.2f,
+           // Face y-
+           +1.0f, -1.0f, +1.0f, 0.2f, 0.8f, 0.2f, -1.0f, -1.0f, +1.0f, 0.2f, 0.8f, 0.2f, +1.0f,
+           -1.0f, -1.0f, 0.2f, 0.8f, 0.2f, +1.0f, -1.0f, -1.0f, 0.2f, 0.8f, 0.2f, -1.0f, -1.0f,
+           +1.0f, 0.2f, 0.8f, 0.2f, -1.0f, -1.0f, -1.0f, 0.2f, 0.8f, 0.2f,
+           // Face y+
+           +1.0f, +1.0f, +1.0f, 0.2f, 0.8f, 0.2f, +1.0f, +1.0f, -1.0f, 0.2f, 0.8f, 0.2f, -1.0f,
+           +1.0f, +1.0f, 0.2f, 0.8f, 0.2f, -1.0f, +1.0f, +1.0f, 0.2f, 0.8f, 0.2f, +1.0f, +1.0f,
+           -1.0f, 0.2f, 0.8f, 0.2f, -1.0f, +1.0f, -1.0f, 0.2f, 0.8f, 0.2f,
+           // Face z-
+           +1.0f, +1.0f, -1.0f, 0.2f, 0.2f, 0.8f, +1.0f, -1.0f, -1.0f, 0.2f, 0.2f, 0.8f, -1.0f,
+           +1.0f, -1.0f, 0.2f, 0.2f, 0.8f, -1.0f, +1.0f, -1.0f, 0.2f, 0.2f, 0.8f, +1.0f, -1.0f,
+           -1.0f, 0.2f, 0.2f, 0.8f, -1.0f, -1.0f, -1.0f, 0.2f, 0.2f, 0.8f,
+           // Face z+
+           +1.0f, +1.0f, +1.0f, 0.2f, 0.2f, 0.8f, -1.0f, +1.0f, +1.0f, 0.2f, 0.2f, 0.8f, +1.0f,
+           -1.0f, +1.0f, 0.2f, 0.2f, 0.8f, +1.0f, -1.0f, +1.0f, 0.2f, 0.2f, 0.8f, -1.0f, +1.0f,
+           +1.0f, 0.2f, 0.2f, 0.8f, -1.0f, -1.0f, +1.0f, 0.2f, 0.2f, 0.8f};
 
-	GLuint vertBufferID;
-	glGenBuffers(1, &vertBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, vertBufferID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);
-	GLint posID = glGetAttribLocation(program.GetPID(), "position");
-	glEnableVertexAttribArray(posID);
-	glVertexAttribPointer(posID, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
-	GLint colID = glGetAttribLocation(program.GetPID(), "color");
-	glEnableVertexAttribArray(colID);
-	glVertexAttribPointer(colID, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-
+    GLuint vertBufferID;
+    glGenBuffers(1, &vertBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);
+    GLint posID = glGetAttribLocation(program.GetPID(), "position");
+    glEnableVertexAttribArray(posID);
+    glVertexAttribPointer(posID, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+    GLint colID = glGetAttribLocation(program.GetPID(), "color");
+    glEnableVertexAttribArray(colID);
+    glVertexAttribPointer(colID, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void *)(3 * sizeof(float)));
 }
 
-void FrameBufferSizeCallback(GLFWwindow* lWindow, int width, int height)
+void FrameBufferSizeCallback(GLFWwindow *lWindow, int width, int height)
 {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
 
 void Init()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GL_FALSE);
-	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Assignment2 - Clint Hart", NULL, NULL);
-	glfwMakeContextCurrent(window);
-	glewExperimental = GL_TRUE;
-	glewInit();
-	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-	glfwSetMouseButtonCallback(window, MouseCallback);
-	glfwSetCursorPosCallback(window, CursorPositionCallback);
-	glfwSetCharCallback(window, CharacterCallback);
-	glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glEnable(GL_DEPTH_TEST);
+    glfwInit();
+    glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GL_FALSE);
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Assignment2 - Clint Hart", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    glewInit();
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetMouseButtonCallback(window, MouseCallback);
+    glfwSetCursorPosCallback(window, CursorPositionCallback);
+    glfwSetCharCallback(window, CharacterCallback);
+    glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
 
-	program.SetShadersFileName(resource_path + "shader.vert", resource_path + "shader.frag");
-	program.Init();
+    program.SetShadersFileName(resource_path + "shader.vert", resource_path + "shader.frag");
+    program.Init();
 
-	CreateCube();
+    CreateCube();
 }
 
-
 int main(int argc, char *argv[])
-{	
-	if (argc < 2)
-	{
-		std::cout << "Need to specify the resource path as the input argument." << std::endl;
-		return 0;
-	}
-	else
-		resource_path = argv[1];
+{
+    if (argc < 2) {
+        std::cout << "Need to specify the resource path as the input argument." << std::endl;
+        return 0;
+    } else
+        resource_path = argv[1];
 
-	Init();
-	while ( glfwWindowShouldClose(window) == 0) 
-	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		Display();
-		glFlush();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    Init();
+    while (glfwWindowShouldClose(window) == 0) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        Display();
+        glFlush();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-	glfwTerminate();
-	return 0;
+    glfwTerminate();
+    return 0;
 }
